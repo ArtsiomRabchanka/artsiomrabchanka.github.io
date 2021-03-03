abd4df9be9743fbf2e8e489da432083639ad6b87
artem <artemr446@gmail.com>
2021-03-03 16:51:28 +0300

    edit code structure

--- ga-script.js
+++ ga-script.js
@@ -275,11 +275,12 @@ gapi.analytics.ready(function() {
                 if(startDate == getMillisecondsTime(response.result.reports[0].data.rows[i].dimensions[1]).getTime()) {
                     data.push(response.result.reports[0].data.rows[i].metrics[0].values[0] * 1);
                     // startDate = getMillisecondsTime(response.result.reports[0].data.rows[i-1].dimensions[1]).getTime();
+                    startDate = startDate + 86400000;
                     console.log('push value')
                 } else {
                     console.log('push 0')
                     data.push(0);
-                    startDate = startDate + 86400000;
+
                     i--;
                 }
             }
