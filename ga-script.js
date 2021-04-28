5dc87bfe1b55ca3e7bb1470e5fbe2a2445d62cc5
artem <artemr446@gmail.com>
2021-04-28 11:10:04 +0300

    number of visits diagram

--- ga-script.js
+++ ga-script.js
@@ -358,19 +358,19 @@ gapi.analytics.ready(function() {
             var startDateY = getMillisecondsTime(response.result.reports[0].data.rows[0].dimensions[1]);
             var startDate = getMillisecondsTime(response.result.reports[0].data.rows[0].dimensions[1]).getTime();
 
-            for(var i=0;i<response.result.reports[0].data.rows.length;i++) {
-                if(startDate == getMillisecondsTime(response.result.reports[0].data.rows[i].dimensions[1]).getTime()) {
-                    data.push(response.result.reports[0].data.rows[i].metrics[0].values[0] * 1);
-                    startDate = getMillisecondsTime(response.result.reports[0].data.rows[i-1].dimensions[1]).getTime();
-                    startDate = startDate + 86400000;
-                    console.log('push value')
-                } else {
-                    console.log('push 0')
-                    data.push(0);
-                    startDate = startDate + 86400000;
-                    i--;
-                }
-            }
+            // for(var i=0;i<response.result.reports[0].data.rows.length;i++) {
+            //     if(startDate == getMillisecondsTime(response.result.reports[0].data.rows[i].dimensions[1]).getTime()) {
+            //         data.push(response.result.reports[0].data.rows[i].metrics[0].values[0] * 1);
+            //         startDate = getMillisecondsTime(response.result.reports[0].data.rows[i-1].dimensions[1]).getTime();
+            //         startDate = startDate + 86400000;
+            //         console.log('push value')
+            //     } else {
+            //         console.log('push 0')
+            //         data.push(0);
+            //         startDate = startDate + 86400000;
+            //         i--;
+            //     }
+            // }
             console.log(data);
 
             // Build the chart
