c0095efad0ec5b943f769a68d4ff443dd36c1e51
artem <artemr446@gmail.com>
2021-04-28 11:01:42 +0300

    number of visits diagram

--- ga-script.js
+++ ga-script.js
@@ -361,7 +361,7 @@ gapi.analytics.ready(function() {
             for(var i=0;i<response.result.reports[0].data.rows.length;i++) {
                 if(startDate == getMillisecondsTime(response.result.reports[0].data.rows[i].dimensions[1]).getTime()) {
                     data.push(response.result.reports[0].data.rows[i].metrics[0].values[0] * 1);
-                    // startDate = getMillisecondsTime(response.result.reports[0].data.rows[i-1].dimensions[1]).getTime();
+                    startDate = getMillisecondsTime(response.result.reports[0].data.rows[i-1].dimensions[1]).getTime();
                     startDate = startDate + 86400000;
                     console.log('push value')
                 } else {
